37ba5d4c267478c65f500de68aaf548d8da011dc
Yong <yong.park123456@gmail.com>
2021-04-20 12:36:52 -0600

    Mostrar 1,2,3 en el cuadro de texto

--- app/src/main/java/com/prueba/myapplication/Calculadora.kt
+++ app/src/main/java/com/prueba/myapplication/Calculadora.kt
@@ -2,10 +2,37 @@ package com.prueba.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class Calculadora : AppCompatActivity() {
+    lateinit var txtResultado: TextView
+    lateinit var  btnUno: Button
+    lateinit var  btnDos: Button
+    lateinit var  btnTres: Button
+    lateinit var  btnMas: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
+
+        txtResultado = findViewById(R.id.textResultado)
+
+        btnUno = findViewById(R.id.btnUno)
+        btnDos = findViewById(R.id.btnDos)
+        btnTres = findViewById(R.id.btnTres)
+        btnMas = findViewById(R.id.btnMas)
+
+        btnUno.setOnClickListener{
+            txtResultado.text = "1"
+        }
+        btnDos.setOnClickListener{
+            txtResultado.text = "2"
+        }
+        btnTres.setOnClickListener{
+            txtResultado.text = "3"
+        }
+
     }
 }
